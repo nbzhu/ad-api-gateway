15ece4d8ab85a7bc03e0b701c42769e7090b2c6d
zj <450453892@qq.com>
2025-09-25 18:53:45 +0800

    广告项目创建按

---
+++ api/ocean/project.go
@@ -0,0 +1,22 @@
+package ocean
+
+import (
+	"context"
+	"fmt"
+	proto "github.com/nbzhu/ad-api-gateway-proto"
+	"github.com/nbzhu/ad-api-gateway/global"
+)
+
+func (s *Api) ProjectCreate(ctx context.Context, req *proto.ProjectCreateReq) (*proto.ProjectCreateResp, error) {
+	body, code, err := global.Http.Post(ctx, "https://api.oceanengine.com/open_api/v3.0/project/create/",
+		req.Params.Body,
+		map[string]string{"Access-Token": s.getAccessToken(ctx)})
+	if err != nil {
+		return nil, err
+	}
+	resp := &proto.ProjectCreateResp{}
+	if err = s.protoJson().Unmarshal(body, resp); err != nil {
+		return nil, fmt.Errorf("反序列化失败:%s。httpCode=%d,原始数据为:%s", err.Error(), code, string(body))
+	}
+	return resp, nil
+}
---
+++ api/ocean/promotion.go
@@ -0,0 +1,22 @@
+package ocean
+
+import (
+	"context"
+	"fmt"
+	proto "github.com/nbzhu/ad-api-gateway-proto"
+	"github.com/nbzhu/ad-api-gateway/global"
+)
+
+func (s *Api) PromotionCreate(ctx context.Context, req *proto.PromotionCreateReq) (*proto.PromotionCreateResp, error) {
+	body, code, err := global.Http.Post(ctx, "https://api.oceanengine.com/open_api/v3.0/promotion/create/",
+		req.Params.Body,
+		map[string]string{"Access-Token": s.getAccessToken(ctx)})
+	if err != nil {
+		return nil, err
+	}
+	resp := &proto.PromotionCreateResp{}
+	if err = s.protoJson().Unmarshal(body, resp); err != nil {
+		return nil, fmt.Errorf("反序列化失败:%s。httpCode=%d,原始数据为:%s", err.Error(), code, string(body))
+	}
+	return resp, nil
+}
